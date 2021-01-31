d6242db902cfd2268a8bcafcbe608e5be8e109a6
CCC <chad@roswellstudios.com>
2021-01-31 16:52:37 -0500

    echo test

--- api/getDeck.js
+++ api/getDeck.js
@@ -1,12 +1,12 @@
-module.exports = getDeck;
+// module.exports = getDeck;
 
-// module.exports = (req, res) => {
-//   res.json({
-//     body: req.body,
-//     query: req.query,
-//     cookies: req.cookies,
-//   })
-// }
+module.exports = (req, res) => {
+  res.json({
+    body: req.body,
+    query: req.query,
+    cookies: req.cookies,
+  })
+}
 
 // app.get(["/deckData/:deckName/", "/deckData/:deckName/*"], getDeck);
 
